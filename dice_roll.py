c77c276ebe3b5fbc1402c964956612edce165bf8
Samuel Massnick <smassnick24@adrian.edu>
2023-03-02 12:57:46 -0500

    added basic dice roll functions and buttons with commands

---
+++ dice_roll.py
@@ -0,0 +1,25 @@
+import random as r
+
+
+def Roll4():
+    return r.randint(1, 4)
+
+
+def Roll6():
+    return r.randint(1, 6)
+
+
+def Roll8():
+    return r.randint(1, 8)
+
+
+def Roll10():
+    return r.randint(1, 10)
+
+
+def Roll12():
+    return r.randint(1,12)
+
+
+def Roll20():
+    return  r.randint(1,20)
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import tkinter as tk
-import random as r
+from dice_roll import *
 
 root = tk.Tk()
+root.geometry("1080x720")
+root.config(background="#4e8bed")
+
+dice4 = tk.Button(root, command=Roll4)
+dice6 = tk.Button(root, command=Roll6)
+dice8 = tk.Button(root, command=Roll8)
+dice10 = tk.Button(root, command=Roll10)
+dice12 = tk.Button(root, command=Roll12)
+dice20 = tk.Button(root, command=Roll20)
+
+root.mainloop()
